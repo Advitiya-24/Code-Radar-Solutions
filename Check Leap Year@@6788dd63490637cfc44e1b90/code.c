d2a1bd029fad2ab99f4edfe0cb424be0d4e88d21
Advitiya Kumar Tyagi <work.advitiya@gmail.com>
2025-02-15 02:35:58 +0530

    Solution to Check Leap Year

---
+++ Check Leap Year@@6788dd63490637cfc44e1b90/code.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    int m;
+    scanf("%d", &m);
+    if((m%4==0 && m%100!=0)||(m%400==0)){
+        printf("Leap Year")
+    }
+    else{
+        printf("Not a Leap Year")
+    }
+}
