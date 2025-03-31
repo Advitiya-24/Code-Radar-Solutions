84cd8a219e1e95f0460d906234fce4de6f3d42e4
Advitiya Kumar Tyagi <work.advitiya@gmail.com>
2025-03-31 23:29:11 +0530

    Solution to Insertion Sort for Integers

---
+++ Insertion Sort for Integers@@67c9a8aa6e98bd154cc82f41/code.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+int compare(const void *a, const void *b) {
+    return (*(int *)a - *(int *)b);
+}
+void insertionSort(int arr[], int n) {
+    qsort(arr, n, sizeof(int), compare);
+}
+
+void printArray(int arr[], int n) {
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
