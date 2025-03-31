cceb021d783c92c8686d68999af100fe356a5b1c
Advitiya Kumar Tyagi <work.advitiya@gmail.com>
2025-04-01 00:10:27 +0530

    Solution to Bubble Sort for Strings

---
+++ Bubble Sort for Strings@@67c9a8aa6e98bd154cc82f40/code.c
@@ -0,0 +1,13 @@
+#include<stdlib.h>
+#include<string.h>
+int compare(const void *a,const void *b){
+    return strcmp((const char *)a,(const char *)b);
+}
+void bubbleSort(char arr[][100],int n){
+    qsort(arr,n,sizeof(arr[0]),compare);
+}
+void printArray(char arr[][100],int n){
+    for(int i=0;i<n;i++){
+        printf("%s \n",arr[i]);
+    }
+}
