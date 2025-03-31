02a3cbb4a0915bd1f22c8b5d7762c2c5d0539a7f
Advitiya Kumar Tyagi <work.advitiya@gmail.com>
2025-03-31 23:54:56 +0530

    Solution to Selection Sort for Strings

--- Selection Sort for Strings@@67ca7acd1fe08d7207c253be/code.c
+++ Selection Sort for Strings@@67ca7acd1fe08d7207c253be/code.c
@@ -3,10 +3,10 @@ int compare(const void *a, const void *b) {
     return (*(char **)a - *(char **)b);
 }
 void selectionSort(char arr[], int n) {
-    qsort(arr, n, sizeof(char), compare);
+    qsort(arr, n, sizeof(char *), compare);
 }
 
-void printArray(char arr[], int n) {
+void printArray(char *arr[], int n) {
     for (int i = 0; i < n; i++) {
         printf("%s ", arr[i]);
     }
