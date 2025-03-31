c8b16895bac40b4a1d4f58b64fc445c5c2f54acc
Advitiya Kumar Tyagi <work.advitiya@gmail.com>
2025-04-01 00:34:55 +0530

    Solution to Insertion Sort for Characters

---
+++ Insertion Sort for Characters@@67c9a8aa6e98bd154cc82f42/code.c
@@ -0,0 +1,12 @@
+#include<stdlib.h>
+int compare(const void *a,const void *b){
+    return(*(char *)a - *(char *)b);
+}
+void insertionSort(char arr[],int n){
+    qsort(arr,n,sizeof(char),compare);
+}
+void printArray(char arr[],int n){
+    for(int i=0;i<n;i++){
+        printf("%c ",arr[i]);
+    }
+}
